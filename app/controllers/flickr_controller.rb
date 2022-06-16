e1912f02ff568fdc9844146b14184b8ab58a925a
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-06-16 10:30:04 -0500

    Handle exceptions generated from flickr api

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,12 +1,25 @@
 class FlickrController < ApplicationController
   def index
-    return unless params[:flickr_user_id]
+    return unless params[:flickr_user_id].present?
 
+    begin
+      @photos = photos_array(params[:flickr_user_id])
+    rescue Flickr::FailedResponse => e
+      flash.now[:alert] = if e.message == "'flickr.people.getPublicPhotos' - User not found"
+                            'User not found!'
+                          else
+                            'Unknown error!'
+                          end
+    end
+  end
+
+  private
+
+  def photos_array(user_id)
     flickr = Flickr.new
+    photos_json = flickr.people.getPublicPhotos(user_id: user_id, per_page: 10)
 
-    photos_json = flickr.people.getPublicPhotos(user_id: '7702423@N04', per_page: 10)
-
-    @photos = photos_json.each_with_object([]) do |photo, array|
+    photos_json.each_with_object([]) do |photo, array|
       array << { thumbnail: Flickr.url_m(photo), full: Flickr.url(photo) }
     end
   end
