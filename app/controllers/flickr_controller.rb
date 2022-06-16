cb03a34b6d751961a658ce65e20e92e0220713a6
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-06-15 21:55:53 -0500

    Add search form
    - Searches a user's public photos
    - populates a list of 5 photo urls to display on the page

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,5 +1,15 @@
 class FlickrController < ApplicationController
   def index
-    
+    if params[:flickr_user_id]
+      # flickr = Flickr.new ENV['flickr_api_key'], ENV['flickr_api_secret']
+      debugger
+      flickr = Flickr.new
+
+      # @photos = flickr.photos.search(tags: params[:flickr_user_id])
+      photos = flickr.people.getPublicPhotos(user_id: '7702423@N04', per_page: 5)
+      @images = photos.map do |url|
+        Flickr.url_m(url)
+      end
+    end
   end
 end
